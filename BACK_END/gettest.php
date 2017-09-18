4b04aa2b6d2eb45d952e6de9ad90e00ed5c71d70
JohnDeignan <32073081+JohnDeignan@users.noreply.github.com>
2017-09-18 15:10:28 -0400

    Create gettest.php

---
+++ BACK_END/gettest.php
@@ -0,0 +1,59 @@
+<?php
+  header('Context-type: application/json');
+  if(isset($_POST['data'])){
+        select();
+        exit(); 
+  }
+?>
+<html>
+	<head>
+		<title>Adam Elshoubri</title>
+	</head>
+	<body>
+		      <form action ='gettest.php' method = POST>
+            		<input type = "hidden" name = "data">
+        	</form>
+
+    		  <?php
+    			  function select(){
+            			mysql_connect('sql1.njit.edu', 'ase28', 'aoFxdVBX3');
+            			mysql_select_db('ase28');	
+	   			        $datastring = $_POST['data'];
+            			$data = json_decode($datastring);          
+            			
+                  $result = mysql_query("SELECT CODE FROM STUDENTS WHERE STUDUSER='$data->user'");
+                  $stduser = mysql_fetch_array($result);
+                  $s_user = $stduser['CODE'];
+                  
+				          $res = mysql_query("SELECT INSTUSER FROM Instructor WHERE CODE='$s_user'");
+                  $stdcode = mysql_fetch_array($res);
+                  $s_code =$stdcode['INSTUSER'];
+				
+				          $res1 = mysql_query("SELECT * FROM INSTTEST WHERE CODE='$s_user'");
+                  $array = array();
+	    			      if($row = mysql_fetch_array($res1)) {
+					          do{
+						            $array[] = $row;
+					          } while($row= mysql_fetch_array($res1));
+	    			      } 
+                  $test = 2;
+                  $size=sizeof($array);
+                  for($i=0;$i<$size;$i++){
+                    $result = mysql_query("SELECT ACTIVE FROM STUDTEST WHERE STUDUSER='$data->user' AND TESTID='".$array[$i][$test]."'");
+                    $act = mysql_fetch_array($result);
+                    $active = $act['ACTIVE'];
+                    $studt = mysql_query("SELECT STUDUSER FROM STUDTEST WHERE TESTID='".$array[$i][$test]."'");
+                    $studtest = mysql_fetch_array($studt);
+                    $stdtest = $studtest['STUDUSER'];
+                    $count = sizeof($stdtest);
+                    array_push($array[$i], $s_code);
+                    array_push($array[$i], $active);
+                    array_push($array[$i], $count);
+                  }
+                  echo json_encode($array);
+			        }
+
+    		    ?>
+
+	</body>
+</html>
