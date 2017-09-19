afb9c37c3819309b5d9b989ff8922a79c3b40938
JohnDeignan <32073081+JohnDeignan@users.noreply.github.com>
2017-09-19 10:51:55 -0400

    Update submitanswer.php

--- BACK_END/submitanswer.php
+++ BACK_END/submitanswer.php
@@ -15,8 +15,8 @@ if(isset($_POST['data'])){
      	  </form>
     		<?php
     			function logInfo(){
-            			mysql_connect('sql1.njit.edu', 'ase28', 'aoFxdVBX3');
-            			mysql_select_db('ase28');
+            			mysql_connect('server', 'username', 'password');
+            			mysql_select_db('dbname');
     			        $datastring = $_POST['data'];
             			$data = json_decode($datastring);
                   $result = mysql_query ("INSERT INTO STUDANSWERS(STUDUSER, TESTID, QUESTID, ANSWERS, SCORE, COMMENTS) VALUES('$data->user','$data->testid'
